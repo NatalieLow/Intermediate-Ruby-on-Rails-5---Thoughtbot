c4b50359bdb092f22380f5b19db0b2c409b0742b
Natalie Low <gnatann@gmail.com>
2020-04-19 15:31:59 -0700

    redirect to dashboard when signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < Clearance::UsersController
- private
 
- def user_params
- 	params.require(:user).permit(:username, :email, :password)
- 	
+	def new
+		@user = User.new
+	end
+
+	# def show
+	# 	@user = User.find(params[:id])
+	# end
+
+	private
+
+	def user_params
+		params
+		.require(:user)
+		.permit(:username, :email, :password)
+	end	
  end
-
-end
