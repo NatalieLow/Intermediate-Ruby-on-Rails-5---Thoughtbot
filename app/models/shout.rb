f18b37f31d742fb4101ea22b999560fce66acf3e
Natalie Low <gnatann@gmail.com>
2020-04-29 16:41:41 -0700

    Added PhotoShouts

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,7 +2,6 @@ class Shout < ApplicationRecord
   belongs_to :user
   belongs_to :content, polymorphic: true
 
-  validates :content, presence: true, length: {in: 1..144}
   validates :user, presence: true
 
   delegate :username, to: :user
