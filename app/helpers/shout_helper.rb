bb297f7af3e20e9d5a7efa40f055b5bfb66f7ac7
Natalie Low <gnatann@gmail.com>
2020-04-30 23:25:10 -0700

    added likes to shouts

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -4,4 +4,12 @@ module ShoutHelper
 		avitar_url = "//www.gravitar.com/avatar#{email_digest}"
 		image_tag  avitar_url
 	end
+
+	def like_button(shout)
+		if current_user.liked?(shout)
+			link_to "Unlike", unlike_shout_path(shout), method: :delete
+		else
+			link_to "Like", like_shout_path(shout), method: :post
+		end
+	end
 end
