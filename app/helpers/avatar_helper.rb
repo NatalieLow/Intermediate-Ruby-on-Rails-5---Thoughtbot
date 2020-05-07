e7d311b05c98e3fd736af858944c1e8be7d0a013
Natalie Low <gnatann@gmail.com>
2020-05-07 10:42:46 -0700

    move avatar helper method to AvatarHelper

---
+++ app/helpers/avatar_helper.rb
@@ -0,0 +1,7 @@
+module AvatarHelper
+	def avatar(user)
+		email_digest = Digest::MD5.hexdigest user.email
+		avitar_url = "//www.gravitar.com/avatar#{email_digest}"
+		image_tag  avitar_url
+	end
+end
