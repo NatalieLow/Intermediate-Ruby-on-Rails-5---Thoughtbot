184eb85daeb874720a00294f8b82e576e09ebce7
Natalie Low <gnatann@gmail.com>
2020-05-11 14:40:57 -0700

    Add ability to follow other users

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,9 +1,4 @@
 module ShoutHelper
-	def avatar(user)
-		email_digest = Digest::MD5.hexdigest user.email
-		avitar_url = "//www.gravitar.com/avatar#{email_digest}"
-		image_tag  avitar_url
-	end
 
 	def like_button(shout)
 		if current_user.liked?(shout)
