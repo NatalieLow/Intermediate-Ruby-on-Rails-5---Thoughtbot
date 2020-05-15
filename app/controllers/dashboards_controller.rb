b4a46c1c94a150f06aed854dc6b22b1b6e29d9b8
Natalie Low <gnatann@gmail.com>
2020-05-14 22:47:20 -0700

    Show followed user shouts on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,6 @@ class DashboardsController < ApplicationController
 
 	def show
 		@shout = Shout.new
-		@shouts = current_user.shouts.order("created_at DESC") 
+		@shouts = current_user.timeline_shouts.order("created_at DESC") 
 	end
 end
